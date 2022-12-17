2f69975839bd34ef2bfd72a863bbdb6090247268
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:22:39 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car("2022 Blue Ferrari");
+        runRace(car);
+
+        Car ferrari = new GasPowerCar("2022 Blue Ferrari", 15.6, 6);
+        runRace(ferrari);
+
+        Car tesla = new ElectricCar("2022 Red Tesla Model 3",
+                568, 75);
+        runRace(tesla);
+
+        Car ferrariHybrid = new HybridCar("2000 Black Ferrari S56 Straddafg", 16,
+                8, 8);
+        runRace(ferrariHybrid);
+    }
+
+    public static void runRace(Car car) {
+
+        car.startEngine();
+        car.drive();
+    }
 }
